a0e5714849efb0dc5c222293006b675d6059c051
Indy Van Canegem <indyvancanegem@hotmail.com>
2021-03-15 00:59:21 +0100

    finished

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <a-scene
-          vr-mode-ui="enabled: false"
-          arjs="sourceType: webcam; debugUIEnabled: false;"
-        >
-          <a-marker preset="hiro">
-            <a-entity
-              position="0 0 0"
-              scale="0.05 0.05 0.05"
-              gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-            ></a-entity>
-          </a-marker>
-          <a-entity camera></a-entity>
-        </a-scene>
+        <iframe
+          className={styles.iframe}
+          srcDoc="<!DOCTYPE html>
+<html>
+  <script src='https://aframe.io/releases/1.0.4/aframe.min.js'></script>
+  <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'></script>
+  <body style='margin: 0px; overflow: hidden'>
+    <a-scene embedded arjs='detectionMode: mono_and_matrix; matrixCodeType: 4x4_BCH_13_9_3'>
+      <a-marker type='barcode' value='105'>
+        <a-entity
+          position='0 0 0'
+          scale='0.02 0.02 0.02'
+          gltf-model='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf'
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  </body>
+</html>
+"
+        ></iframe>
       </main>
     </div>
   );
