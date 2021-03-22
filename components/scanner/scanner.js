74bc6d1deb1d462ff80b13cabca43f3ee04b22ec
Indy Van Canegem <indyvancanegem@hotmail.com>
2021-03-22 23:57:30 +0100

    model placement is correct now

--- components/scanner/scanner.js
+++ components/scanner/scanner.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./scanner.module.scss";
 
@@ -6,34 +6,52 @@ import { ARnft } from "@kalwalt/ar-nft";
 import * as THREE from "three";
 
 export default function Scanner() {
+  const [video, setVideo] = useState(null);
+  const [canvas, setCanvas] = useState(null);
+
+  useEffect(() => {
+    if (video) {
+      video.style.position = "absolute";
+      video.style.top = 0;
+      video.style.left = 0;
+    }
+  }, [video]);
+
+  useEffect(() => {
+    if (canvas) {
+      canvas.style.position = "absolute";
+      canvas.style.top = 0;
+      canvas.style.left = 0;
+    }
+  }, [canvas]);
+
   useEffect(() => {
     ARnft.init(640, 480, "marker/QR", "config.json", false)
       .then((nft) => {
         document.addEventListener("onInitThreejsRendering", (ev) => {
-          console.log(ev.detail);
           const renderer = ev.detail.renderer;
-          console.log(renderer);
           const scene = ev.detail.scene;
-          console.log(scene);
           const camera = ev.detail.camera;
-          console.log(camera);
           renderer.outputEncoding = THREE.sRGBEncoding;
           renderer.physicallyCorrectLights = true;
           let directionalLight = new THREE.DirectionalLight("#fff", 0.4);
           directionalLight.position.set(0.5, 0, 0.866);
           scene.add(directionalLight);
+          setCanvas(document.getElementById("canvas"));
+          setVideo(document.getElementById("video"));
         });
-        //model, x, y, z, scale
-        nft.addModel("brave_robot.glb", 0, 0, 0, 80);
-
-        const canvas = document.getElementById("canvas");
-        const video = document.getElementById("video");
-        video.style.position = "absolute";
-        video.style.top = 0;
-        video.style.left = 0;
-        canvas.style.position = "absolute";
-        canvas.style.top = 0;
-        canvas.style.left = 0;
+        document.addEventListener("getNFTData", (e) => {
+          console.log("details", e.detail);
+          const marker = e.detail;
+          //model, x, y, z, scale
+          nft.addModel(
+            "brave_robot.glb",
+            ((marker.width / marker.dpi) * 2.54 * 10) / 2.0,
+            ((marker.height / marker.dpi) * 2.54 * 10) / 2.0,
+            100,
+            100
+          );
+        });
       })
       .catch((error) => {
         console.log(error);
